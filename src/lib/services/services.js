802fc2eb2ce357f4e82cf239e4ca65fdace624b0
jhtong <me@joeltong.org>
2019-03-16 14:03:19 +0800

    andded model

--- src/lib/services/services.js
+++ src/lib/services/services.js
@@ -91,6 +91,10 @@ const saveFollowerToQueue = async login => {
   );
 };
 
+/**
+ * Run load repo service
+ * @param {*} param0
+ */
 export const runLoadRepoFollowersService = ({
   timeInterval = 2000,
   numWorkers = 1
@@ -102,6 +106,64 @@ export const runLoadRepoFollowersService = ({
     async () => {
       // Find a repo to update
       const repoQueue = await RepoQueue.findOne({});
+
+      // skip if queue empty
+      if (!repoQueue) {
+        return;
+      }
+
+      // delete from queue
+      await RepoQueue.deleteOne({ _id: repoQueue._id });
+
+      const fullName = repoQueue.fullName.toLowerCase();
+      console.log(`Scraping ${fullName}..`);
+
+      // Scrape the repo
+      const repoInfo = await scrapeRepo({
+        repo: fullName,
+        maxPages: 10,
+        skipFollowers: false
+      });
+
+      // skip if no such repo
+      if (!repoInfo) {
+        return;
+      }
+
+      // save followers
+      const { followers } = repoInfo;
+      const saveFollowerPromises = followers.map(f => {
+        const login = f.handle.toLowerCase();
+        return saveFollowerToQueue(login);
+      });
+
+      console.log(
+        `Saving ${saveFollowerPromises.length} profiles from repo=${fullName}..`
+      );
+
+      await Promise.all(saveFollowerPromises);
+    }
+  );
+
+  // run cluster
+  cluster.map(s => s());
+};
+
+/**
+ * Run load query service
+ * @param {*} param0
+ */
+export const runLoadQueryService = ({
+  timeInterval = 2000,
+  numWorkers = 1
+}) => {
+  const cluster = genServiceCluster(
+    'runLoadQueryService',
+    timeInterval,
+    numWorkers,
+    async () => {
+      // Find a repo to update
+      const QueryQueue = await QueryQueue.findOne({});
 
       // skip if queue empty
       if (!repoQueue) {
