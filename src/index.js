0caed68016fc8f95346e7197382a5a32b767cc54
jhtong <me@joeltong.org>
2019-03-28 22:39:27 +0800

    exposed num workers as env parameter

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {
 } from './lib/services/services';
 import Profile from './lib/models/Profile';
 
-const { MONGO_URI } = process.env;
+const { MONGO_URI, NUM_WORKERS_USER_UPDATE = 3 } = process.env;
 
 // connect to Mongo
 mongoose.connect(MONGO_URI);
@@ -26,7 +26,7 @@ export default function app() {
   console.log('Running Scraper..');
   runUpdateUserService({
     timeInterval: 2000,
-    numWorkers: 2 // usual 5
+    numWorkers: parseInt(NUM_WORKERS_USER_UPDATE, 10) // usual 5
   });
 
   runLoadRepoFollowersService({
